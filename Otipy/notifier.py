f6da8ece2b92c4f50d7ec0098ebc98cbcf5d8c54
Mahima Tripathi <mahimatripathi0625@gmail.com>
2023-11-09 15:38:01 +0530

    updated

--- Otipy/notifier.py
+++ Otipy/notifier.py
@@ -1,44 +1,13 @@
-# # notifier.py
-
-# import smtplib
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-
-# def send_notification(subject, message):
-#     from_email = "nidhi22inmas@gmail.com"
-#     to_email = "kdhini2807@gmail.com"
-#     password = "hhfaizgjcaqpsjwl"
-
-#     msg = MIMEMultipart()
-#     msg["From"] = from_email
-#     msg["To"] = to_email
-#     msg["Subject"] = subject
-
-#     body = message
-#     msg.attach(MIMEText(body, "plain"))
-
-#     server = smtplib.SMTP("smtp.gmail.com", 587)
-#     server.starttls()
-#     server.login(from_email, password)
-#     server.sendmail(from_email, to_email, msg.as_string())
-#     server.quit()
-    
-# def send_test_notification():
-#     subject = "Test Notification"
-#     message = "This is a test notification. If you receive this, your notifier is working correctly!"
-#     send_notification(subject, message)
-
-
-
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+import schedule
+import time
 def send_notification(subject, message_html, recipients):
     # Email configuration
-    sender_email = 'nidhi22inmas@gmail.com'
-    sender_password = 'hhfaizgjcaqpsjwl'
+    sender_email = 'mahimatripathi0625@gmail.com'
+    sender_password = 'fvhzimtbnbnhairi'
     smtp_server = 'smtp.gmail.com'
     smtp_port = 587  # Port for Gmail SMTP
 
@@ -73,30 +42,11 @@ def send_notification(subject, message_html, recipients):
 # Usage example
 if __name__ == "__main__":
     subject = "New Products Alert"
-    recipients = ['kdhini2807@gmail.com']
+    recipients = ['rprithvi786@gmail.com']
 
     # Example table HTML (replace this with your actual table HTML)
     table_html = """
-    <table border="1">
-        <tr>
-            <th>Product Name</th>
-            <th>Original Price</th>
-            <th>Discounted Price</th>
-            <th>Discount</th>
-        </tr>
-        <tr>
-            <td>Product 1</td>
-            <td>$10.00</td>
-            <td>$8.00</td>
-            <td>20%</td>
-        </tr>
-        <tr>
-            <td>Product 2</td>
-            <td>$15.00</td>
-            <td>$12.00</td>
-            <td>20%</td>
-        </tr>
-    </table>
+   
     """
 
     # Your message containing the table HTML
